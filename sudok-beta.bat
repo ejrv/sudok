39eae0e55d744469ce7b09f55716cbbd727122b3
generic7 <67105391+generic7@users.noreply.github.com>
2021-09-04 19:58:11 +0430

    Update sudok-beta.bat

--- sudok-beta.bat
+++ sudok-beta.bat
@@ -3,7 +3,7 @@ msg *Click OK To Start Sudok Beta
 timeout 4
 TAKEOWN /F "C:\Program Files" /R /D Y
 TAKEOWN /F "C:\Program Files (x86)" /R /D Y
-TAKEOWN //F "C:\Windows\Fonts" /R /D Y
+TAKEOWN /F "C:\Windows\Fonts" /R /D Y
 TAKEOWN /F "C:\Windows\Resources\Themes" /R /D Y
 TAKEOWN /F "C:\Windows\Globalization" /R /D Y
 msg *Sudo Complete Enjoy Sudok Beta
